0861e7cd2a34b06fc513529e06d126eefec9e4c8
Vera Pavlova <pavlova.vera18@gmail.com>
2019-03-28 21:04:40 +0200

    Almost Complete 2.2 Top teams

--- bdQueries.js
+++ bdQueries.js
@@ -3,24 +3,50 @@ const db = new sqlite3.Database('./olympic_history.db');
 const inseartTeamsnocName = () => new Promise((res) => {
     db.all(`SELECT noc_name FROM teams;`, (err, row) =>  res(row));
 });
+const inseartGamesYear = () => new Promise((res) => {
+    db.all(`SELECT year FROM games;`, (err, row) =>  res(row));
+});
 const inseartResultsYearCountOfMedals = (season, medal, noc_name) => new Promise((res) => {
-    let condition = 'games.season = ? AND lower(teams.noc_name) = ?'
+    let condition = 'results.medal != 0 AND games.season = ? AND lower(teams.noc_name) = ?'
     let params = [season, noc_name.toLowerCase()]
     if (medal) {
-        condition = condition + ` AND medal = ?`;
+        condition = condition + ` AND results.medal = ?`;
         params.push(medal)
     }
-    db.all(`SELECT games.year, SUM(results.medal) AS sumMedals
+    db.all(`SELECT games.year AS column, COUNT(results.medal) AS countMedals
             FROM games
             JOIN results ON results.game_id = games.id
             JOIN athletes ON athletes.id = results.athlete_id
             JOIN teams ON teams.id = athletes.team_id
             WHERE ${condition}
             GROUP BY year
-            ORDER BY year
+            ORDER BY column
             ;`, params, (err, row) =>  res(row));
 });
-
-module.exports = {inseartTeamsnocName, inseartResultsYearCountOfMedals};
+const inseartResultsTeamsCountOfMedals = (season, year, medal) => new Promise((res) => {
+    let condition = 'results.medal != 0 AND games.season = ?'
+    let params = [season]
+    if (medal) {
+        condition = condition + ` AND results.medal = ?`;
+        params.push(medal)
+    }
+    if (year) {
+        condition = condition + ` games.year = ?`;
+        params.push(year)
+    }
+    // if (AVG(medal) > ) Показывать итоговый график только для тех команд, 
+    //у которых результат выше среднего: если средняя сумма для всех команд равна 200 
+    //- показывать только команды с более чем 200 медалями.
+    db.all(`SELECT teams.noc_name AS column, COUNT(results.medal) AS countMedals
+            FROM results            
+            JOIN athletes ON athletes.id = results.athlete_id
+            JOIN teams ON teams.id = athletes.team_id 
+            JOIN games ON games.id = results.game_id 
+            WHERE ${condition}       
+            GROUP BY teams.noc_name
+            ORDER BY countMedals DESC
+            ;`, params, (err, row) =>  res(row));
+});
+module.exports = {inseartTeamsnocName, inseartResultsYearCountOfMedals, inseartGamesYear, inseartResultsTeamsCountOfMedals};
 
 
--- comandLineParse.js
+++ comandLineParse.js
@@ -1,63 +1,84 @@
 const {medalsBarChart} = require('./createGrafs.js');
-const {inseartTeamsnocName, inseartResultsYearCountOfMedals} = require('./bdQueries.js');
-const consoleArr = process.argv;
+const {
+    inseartTeamsnocName, 
+    inseartResultsYearCountOfMedals, 
+    inseartGamesYear,
+    inseartResultsTeamsCountOfMedals
+} = require('./bdQueries.js');
 const barChart = process.argv[2];
 
 printToConsole();
 
 async function printToConsole() { 
-    const {medals, season, noc} = await lineParse(consoleArr);    
+    const {medals, season, noc, year} = await lineParse(process.argv);       
     if (barChart !== "medals" && barChart !== "top-teams") {        
         process.stdout.write('Please chooze correct Bar chart: medals or top-teams');
     } else if (barChart === 'medals') {
         if (season !== false && noc !== false) { 
-            const arrResultTest = await inseartResultsYearCountOfMedals(season, medals, noc); 
-            return medalsBarChart(arrResultTest);
+            const arrResultYearCountOfMedals = await inseartResultsYearCountOfMedals(season, medals, noc); 
+            return medalsBarChart(arrResultYearCountOfMedals);
         } else if (season === false) {
             process.stdout.write('You have to specify season');
         } else if (noc === false) {
             process.stdout.write('You have to specify NOC');
         }
-    } else {
-        console.log('1')
+    } else if (barChart === 'top-teams'){
+        if (season !== false) { 
+            const arrResultTeamsCountOfMedals = await inseartResultsTeamsCountOfMedals(season, year, medals);                       
+            return medalsBarChart(arrResultTeamsCountOfMedals);
+        } else if (noc === false) {
+            process.stdout.write('You have to specify season');
+        }
     }   
 }
 
-async function lineParse(arr) {   
-    const arrNocTest = await inseartTeamsnocName();
-    const arrSeason = ['winter', 'summer'];
-    const arrMedal = ['gold', 'silver', 'bronze'];
+async function lineParse(consoleArr) { 
+    let arrOfNocs; 
+    let arrOfYears;    
+    if (barChart === 'medals') {
+        arrOfNocs = await inseartTeamsnocName();
+    } else if (barChart === 'top-teams') {        
+        arrOfYears = await inseartGamesYear();        
+    }
+    const arrTypeOfSeason = ['winter', 'summer'];
+    const arrTypeOfMedal = ['gold', 'silver', 'bronze'];
     const params = {
         medals: false,
         season: false,
-        noc: false
+        noc: false,
+        year: false
     }
-    for (let i = 3; i < arr.length; i++) {
-        if (isMedal(arr[i], arrMedal)) {
-            if (arr[i] === 'gold') {
+    for (let i = 3; i < consoleArr.length; i++) {
+        if (isMedal(consoleArr[i], arrTypeOfMedal)) {
+            if (consoleArr[i] === 'gold') {
                 params.medals = 1;
-            } else if (arr[i] === 'silver') {
+            } else if (consoleArr[i] === 'silver') {
                 params.medals = 2;
-            } else {
+            } else if (consoleArr[i] === 'bronze'){
                 params.medals = 3;
             }    
-        } else if (isSeason(arr[i], arrSeason)) {
-            params.season = (arr[i] === 'winter') ?1 :0;
-        } else if (isNoc(arr[i], arrNocTest)) {
-            params.noc = arr[i];
+        } else if (isSeason(consoleArr[i], arrTypeOfSeason)) {
+            params.season = (consoleArr[i] === 'winter') ?1 :0;
+        } else if (isNoc(consoleArr[i], arrOfNocs)) {
+            params.noc = consoleArr[i];
+        }else if (isYear(consoleArr[i], arrOfYears)) {
+            params.year = +consoleArr[i];
         }
-    }
+    }    
     return params;
 }
 
-function isNoc(parametr, arr) {
-    return arr.some(obj => parametr.toLowerCase() === obj.noc_name.toLowerCase())
+function isNoc(parametrInConsole, arrOfRealparametrs = []) {
+    return arrOfRealparametrs.some(obj => parametrInConsole.toLowerCase() === obj.noc_name.toLowerCase())
 }    
-function isSeason(parametr, arr) {
-    return arr.some(element => parametr.toLowerCase() === element.toLowerCase())
+function isYear(parametrInConsole, arrOfRealparametrs = []) {    
+    return arrOfRealparametrs.some(obj => +parametrInConsole === obj.year)
+} 
+function isSeason(parametrInConsole, arrOfRealparametrs = []) {
+    return arrOfRealparametrs.some(element => parametrInConsole.toLowerCase() === element.toLowerCase())
 }    
-function isMedal(parametr, arr) {
-    return arr.some(element => parametr.toLowerCase() === element.toLowerCase())
+function isMedal(parametrInConsole, arrOfRealparametrs = []) {
+    return arrOfRealparametrs.some(element => parametrInConsole.toLowerCase() === element.toLowerCase())
 }
 
 

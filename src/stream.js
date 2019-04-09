d210f8fca00b39d45261b6c990bf2946308f1e92
Vera Pavlova <pavlova.vera18@gmail.com>
2019-04-09 20:42:57 +0300

    Code style refactoring 4.2

--- src/stream.js
+++ src/stream.js
@@ -5,20 +5,15 @@ const { ProcessLineToDB } = require('./processLinetoDB');
 const toOlympicHistory = new ProcessLineToDB();
 
 async function streamOn() {
-  const stream = fs.createReadStream('./athlete_events.csv', {
-    highWaterMark: 16,
-  });
+  const stream = fs.createReadStream('./athlete_events.csv');
   let buffer = '';
 
   for await (const chunk of stream) {
-    stream.pause();
     buffer += chunk;
     if (buffer.indexOf('\n') !== -1 && buffer !== undefined) {
       const arr = buffer.split('\n');
       buffer = arr.pop();
-      Promise.all(arr.map(row => toOlympicHistory.processLine(row))).then(() => stream.resume());
-    } else {
-      stream.resume();
+      Promise.all(arr.map(row => toOlympicHistory.processLine(row)));
     }
   }
 }
